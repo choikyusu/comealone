4e50680727174c9ff83da1a7bfbd6ffa57a2a755
DESKTOP-DJFQRCJ\최규수 <kyusuone@gmail.com>
2021-04-30 22:24:54 +0900

    네이버 로그인

--- src/NaverLogin.jsx
+++ src/NaverLogin.jsx
@@ -1,29 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 
-class NaverLogin extends React.Component {
-  constructor(props) {
-    super(props);
-    this.initializeNaverLogin = this.initializeNaverLogin.bind(this);
-  }
-
+class NaverLogin extends Component {
   componentDidMount() {
-    const script = document.createElement("script");
+    // Naver sdk import
+    const naverScript = document.createElement("script");
+    naverScript.src =
+      "https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.2.js";
+    naverScript.type = "text/javascript";
+    document.head.appendChild(naverScript);
 
-    script.src = "https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.0.js";
-    script.async = true;
+    // Naver sdk 스크립트 로드 완료시
+    naverScript.onload = () => {
+      const naverLogin = new window.naver.LoginWithNaverId({
+        clientId: "Naver Client ID",
+        callbackUrl: "로그인 성공이후 URL",
+        callbackHandle: true,
+        isPopup: false, // 로그인 팝업여부
+        loginButton: {
+          color: "green", // 색상(white, green)
+          type: 3, // 버튼타입(1,2,3)
+          height: 60, // 배너 및 버튼 높이
+        },
+      });
 
-    document.body.appendChild(script);
-  }
+      naverLogin.init();
+      naverLogin.logout(); // 네이버 로그인이 계속 유지되는 경우가 있다. 초기화시 로그아웃
+      naverLogin.getLoginStatus((status) => {
+        if (status) {
+          console.log("Naver 로그인 상태", naverLogin.user);
+          const { id, email, gender } = naverLogin.user;
 
-  initializeNaverLogin() {
-    const naver = window["naver"];
-    const naverLogin = new naver.LoginWithNaverId({
-      clientId: "O5PQk7N5HMqCty9Yeb2V",
-      callbackUrl: "http://localhost:3000",
-      isPopup: false, // popup 형식으로 띄울것인지 설정
-      loginButton: { color: "white", type: 1, height: "47" }, //버튼의 스타일, 타입, 크기를 지정
-    });
-    naverLogin.init();
+          // 필수 제공 동의 조건
+          if (gender == undefined) {
+            alert("성별은 필수 동의 입니다. 정보제공을 동의해주세요.");
+            naverLogin.reprompt();
+            return;
+          }
+        } else {
+          console.log("Naver 비 로그인 상태");
+        }
+      });
+    };
   }
 
   render() {
