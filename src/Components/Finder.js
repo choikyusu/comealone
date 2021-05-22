5a250d4a631826ee7aa144a0c0f89112d8069333
HEC\2025938 <kyusu88@naver.com>
2021-05-22 20:48:39 +0900

    해더 추가

--- src/Components/Finder.js
+++ src/Components/Finder.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
-
+import styles from './css/Finder.css'
 
 function Finder ({userData, accessToken}){
 
     return (
-    <div>
-        
+    <div className="header-container">
+        <div className="header-section">
+            <div className="search-box-wrapper">
+                <div className="search-box-search-btn">
+                    <img src={require("../assets/images/search.svg").default } alt="search"/>
+                </div>
+                <div className="search-box-query-box">
+                    <input type="text" className="search-query" placeholder="검색해보세요.">
+
+                    </input>
+                </div>
+            </div>
+
+        </div>
     </div>);
 }
 
